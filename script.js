f169c35cd755ebb6b8555d3bfde2740fd5493f60
gribiismail6-stack <gribiismail6@gmail.com>
2026-08-07 11:52:25 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+const scriptURL = "https://script.google.com/macros/s/AKfycbw4NNsXr8V4mjd-RzQVGpJJGgbukYESFjtmYGoOZ3rPd0m4eWrfK8jZZDdC9ZStfXHq/exec";
+
+const form = document.getElementById("orderForm");
+const successMessage = document.getElementById("successMessage");
+
+form.addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    const data = {
+        name: document.getElementById("name").value,
+        phone: document.getElementById("phone").value,
+        wilaya: document.getElementById("wilaya").value,
+        address: document.getElementById("address").value,
+        quantity: document.getElementById("quantity").value
+    };
+
+
+    fetch(scriptURL, {
+        method: "POST",
+        body: JSON.stringify(data)
+    })
+
+    .then(response => response.json())
+
+    .then(result => {
+
+        successMessage.style.display = "block";
+        successMessage.innerHTML = "✅ تم إرسال طلبك بنجاح";
+
+        form.reset();
+
+        setTimeout(function(){
+            successMessage.style.display = "none";
+        }, 4000);
+
+    })
+
+    .catch(error => {
+
+        console.log("Error:", error);
+        alert("حدث خطأ أثناء إرسال الطلب");
+
+    });
+
+});
